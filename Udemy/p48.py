6d80baf58d70f94e6498b3efe5d048bc88352198
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-03-19 15:13:10 +0000

    48 is done

--- Udemy/p48.py
+++ Udemy/p48.py
@@ -1,3 +1,6 @@
+import random
+
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +27,41 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+user_hand_print = ""
+com_hand_print =  ""
+
+random_hand = random.randint(0,2)
+
+if random_hand == 0:
+  com_hand_print = rock
+elif random_hand == 1:
+  com_hand_print = paper
+elif random_hand == 2:
+  com_hand_print = scissors
+
+  
+
+user_hand = int(input("Enter 0 = Rock, 1 = paper and 2 = scissors:"))
+
+if user_hand == 0:
+  user_hand_print = rock
+elif user_hand == 1:
+  user_hand_print = paper
+elif user_hand == 2:
+  user_hand_print = scissors
+else:
+  print("Invalid input")
+  break
+
+print("Computer chose:", com_hand_print)
+print("user chose:", user_hand_print)
+
+if user_hand == 0 and random_hand == 1:
+  print("Computer wins")
+elif user_hand == 1 and random_hand == 2:
+  print("Computer wins")
+elif user_hand == random_hand:
+  print("Draw")
+else:
+  print("User wins")
