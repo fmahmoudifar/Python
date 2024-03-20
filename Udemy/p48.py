961c075c459f9a3a2c43c3a5f6e83e5387e366e3
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-03-20 09:38:36 +0000

    updated 48

--- Udemy/p48.py
+++ Udemy/p48.py
@@ -33,17 +33,21 @@ com_hand = random.randint(0, 2)
 
 user_hand = int(input("Enter 0 = Rock, 1 = paper and 2 = scissors:"))
 
-print("user chose: ")
-print(gane_images[user_hand])
-
-print("Computer chose: ")
-print(gane_images[com_hand])
-
-if user_hand == 0 and com_hand == 1:
-  print("Computer wins")
-elif user_hand == 1 and com_hand == 2:
-  print("Computer wins")
-elif user_hand == com_hand:
-  print("Draw")
+if user_hand >= 3:
+  print("You hav etyped an invalid number, you lose!")
 else:
-  print("User wins")
+  print("You chose: ")
+  
+  print(gane_images[user_hand])
+  
+  print("Computer chose: ")
+  print(gane_images[com_hand])
+  
+  if user_hand == 0 and com_hand == 1:
+    print("You lose")
+  elif user_hand == 1 and com_hand == 2:
+    print("You Lose")
+  elif user_hand == com_hand:
+    print("It is a Draw")
+  else:
+    print("You win!")
