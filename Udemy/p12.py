e829b96c56fc036ce2e1ea4eb6093c53adbbc490
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-10-31 20:30:32 +0000

    add 12

---
+++ Udemy/p12.py
@@ -0,0 +1,38 @@
+import random
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return 10
+    else:
+        return 5
+
+def check_answer(guess, answer, turns):
+    if guess > answer:
+        print("Too high.")
+        return turns - 1
+    elif guess < answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {answer}.")
+
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = random.randint(1, 100)
+    print(f"Pssst, the correct answer is {answer}")
+    turns = set_difficulty()
+    guess = 0
+
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print(f"You've run out of guesses, you lose. the number was {answer}")
+            return
+        elif guess != answer:
+            print("Guess again")
+
+game()
