55c480b8a1dafb3deb356ae3504013ea1b96c9ca
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-11-14 06:57:18 +0200

    update 14

--- Udemy/p14.py
+++ Udemy/p14.py
@@ -1,4 +1,4 @@
-from Udemy.p14data import data
+from p14data import data
 import random
 
 def format_data(account):
@@ -7,6 +7,11 @@ def format_data(account):
   account_country = account["country"]
   return f"{account_name}, a {account_descr}, from {account_country}"
 
+def check_answer(user_guess, a_follower, b_follower):
+  if a_follower > b_follower:
+    return user_guess == 'a'
+  else:
+    return user_guess == 'b'
 
 account_a = random.choice(data)
 account_b = random.choice(data)
@@ -14,4 +19,17 @@ if account_a == account_b:
     account_b = random.choice(data)
 
 print(f"Compare A: {format_data(account_a)}.")
+print("VS")
 print(f"Compare B: {format_data(account_b)}.")
+
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+if is_correct:
+  print("You are right!")
+else:
+  print("Sorry, that's wrong.")
