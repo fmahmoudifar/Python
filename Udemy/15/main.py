a02033eeb3b2ff101ac006ace7b88b2c78ece701
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-11-18 23:29:48 +0200

    add

---
+++ Udemy/15/main.py
@@ -0,0 +1,3 @@
+Menu ={
+MENU = { 2 ►venv "espresso": { 3 main.py A Ill External Libraries "ingredients": { "water": 50. 5 "coffee": 18. Scratches and Consoles 6 }, 7 "cost": 1.5, } "latte": { 19 "ingredients": { 11 "water": 200, "milk": 158, 18 "coffee": 24, 14 }, 15 "cost": 2.5, 16 }, 17 "cappuccino": { "ingredients": { 19 "water: 258. 2 21 "milk": 108, "coffee": 24, 22 },
+}
