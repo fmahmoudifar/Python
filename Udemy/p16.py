38bb63898ba04fe70a9d7a1f230f6c48f1132f55
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-11-23 20:36:53 +0200

    update

---
+++ Udemy/p16.py
@@ -0,0 +1,11 @@
+import turtle
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.color("blue")
+timmy.shape("turtle")
+timmy.forward(100)
+ 
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
