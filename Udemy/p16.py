e945d3aa97487a43a664c6f27df43426f4cd11df
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2024-11-23 22:47:14 +0200

    add 16, remove 15 folder

--- Udemy/p16.py
+++ Udemy/p16.py
@@ -1,11 +1,19 @@
-import turtle
+# import turtle
 
-timmy = turtle.Turtle()
-print(timmy)
-timmy.color("blue")
-timmy.shape("turtle")
-timmy.forward(100)
+# timmy = turtle.Turtle()
+# print(timmy)
+# timmy.color("blue")
+# timmy.shape("turtle")
+# timmy.forward(100)
  
-my_screen = turtle.Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Name 1", "Name 2", "Name 3"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "L"
+print(table)
