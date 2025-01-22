61013a8df4384d2bbc9db5293374f503b66c48be
Farshad Mahmoudifar <FMahmoudifar@ne.linedata.com>
2025-01-22 23:56:13 +0200

    Your commit message here

---
+++ Udemy/p24.py
@@ -0,0 +1,9 @@
+# file = open("p24.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+with open("p24.txt") as file:
+  content = file.read()
+  print(content)
+
---
+++ Udemy/p24/main.py
@@ -0,0 +1,14 @@
+# file = open("my_file.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+# with open("my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+# with open("my_file.txt" , "w") as file:
+#     file.write("New text.")
+
+with open("my_file.txt" , "a") as file:
+    file.write("\nNew text.")
