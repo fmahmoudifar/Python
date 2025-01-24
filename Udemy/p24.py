c8bd34081756e170f09863c2229453d59ca7ccab
Farshad Mahmoudifar <FMahmoudifar@ne.linedata.com>
2025-01-24 15:20:48 +0200

    update p24

--- Udemy/p24.py
+++ Udemy/p24.py
@@ -3,8 +3,12 @@
 # print(content)
 # file.close()
 
-with open("p24.txt") as file:
-  content = file.read()
-  print(content)
+# with open("p24.txt") as file:
+#     content = file.read()
+#     print(content)
 
-test
+# with open("p24.txt" , "w") as file:
+#     file.write("New text.")
+
+with open("p24.txt" , "a") as file:
+    file.write("\nNew text.")
