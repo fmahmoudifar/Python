81ecea393d36119dd22f1e82a2da3167d63d3936
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2025-04-28 00:07:05 +0300

    update

--- Udemy/p25/main1.py
+++ Udemy/p25/main1.py
@@ -2,12 +2,22 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
 
-with open("Udemy/p25/weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temp = []
-    for row in data:
-        if row[1] != "temp":
-            temp.append(int(row[1]))
-    print(temp)
+#######################
+
+# import csv
+
+# with open("Udemy/p25/weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temp = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temp.append(int(row[1]))
+#     print(temp)
+
+############################
+
+import pandas
+
+data = pandas.read_csv("Udemy/p25/weather_data.csv")
+print(data["temp"])
