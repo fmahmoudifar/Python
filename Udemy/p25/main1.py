318128b9fb13e8e42a9ade5ce8858dea07be87f3
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2025-05-01 09:02:51 +0300

    update

--- Udemy/p25/main1.py
+++ Udemy/p25/main1.py
@@ -21,3 +21,34 @@ import pandas
 
 data = pandas.read_csv("Udemy/p25/weather_data.csv")
 print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+
+
+
+avg = sum(temp_list) / len(temp_list)
+print(avg)
+
+print(data["temp"].mean())
+
+print(data["temp"].max())
+
+print(data.condition)
+
+print(data[data.day == 'Monday'])
+
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == 'Monday']
+print(monday.condition)
+
+####################################
+
+data = pandas.read_csv("Udemy/p25/2018.csv")
+gray_squirrels = data[data["Primary Fur Color"] == "Gray"]
+print(gray_squirrels)
